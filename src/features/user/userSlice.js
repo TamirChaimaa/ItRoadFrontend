73fec4000f588d722b9fe7572b6b37e5305d3d63
Tamirchaimaa <chaimaatamir43@gmail.com>
2025-07-13 10:55:51 +0100

    Add  https

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,7 +2,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import Cookies from 'js-cookie';
-const API_BASE_URL = 'http://itroadusersservice-production.up.railway.app/api/users';
+const API_BASE_URL = 'https://itroadusersservice-production.up.railway.app/api/users';
 
 // Async thunk to fetch current user profile
 export const fetchCurrentUserProfile = createAsyncThunk(
