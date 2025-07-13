f60f99d3b0e978536d3d15cb03888c51c7be39b1
Tamirchaimaa <chaimaatamir43@gmail.com>
2025-07-13 13:49:23 +0100

    change teh footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-white border-t border-gray-200 px-6 py-4">
       <div className="flex flex-col md:flex-row justify-between items-center">
         <p className="text-sm text-gray-600">
-          © 2024 IT ROAD GROUP. All rights reserved.
+          © 2025 IT ROAD GROUP. All rights reserved.
         </p>
       </div>
     </footer>
